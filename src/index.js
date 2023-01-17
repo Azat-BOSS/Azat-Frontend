8449840f1c502a59bb7eaccad966cf5dac9a40a0
Азат Гайнутдинов <azatgajnutdinov@MacBook-Air-Azat.local>
2023-01-17 15:30:11 +0300

    fix store

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
-import { store } from './Services/store';
+import { store } from './Services/store.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/services/store.js
+++ src/services/store.js
@@ -1,4 +1,4 @@
-import {configureStore} from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit"
 import modalSlice from "./modalReducer"
 
 export const store = configureStore({
